36bf4ff6e5476cc162c88f40d531f48f722aa530
rv602 <rajagopalan602@gmail.com>
2023-04-29 21:13:55 +0530

    nextron init

---
+++ app/renderer/pages/home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <button className="bg-green-300 p-3 rounded-md">hmm</button>
+    </div>
+  )
+}
