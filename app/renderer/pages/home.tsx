e7171d7cea44d5cfb603353b825052743822d905
Abhinavxox <abhinavxox@gmail.com>
2023-04-29 21:57:09 +0530

    <Added>:[Script run from App]

--- app/renderer/pages/home.tsx
+++ app/renderer/pages/home.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import { shell } from "electron";
+const { exec } = require("child_process");
 
 export default function Home() {
+  const runPythonScript = () => {
+    exec("python3 scripts/python_environments.py", (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+  };
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <button className="bg-green-300 p-3 rounded-md">hmm</button>
+      <button className="bg-green-300 p-3 rounded-md" onClick={runPythonScript}>
+        Run Python Script
+      </button>
     </div>
-  )
+  );
 }
---
+++ app/scripts/node_environment.py
@@ -0,0 +1,48 @@
+import json
+import os
+import platform
+import subprocess
+import base64
+
+system = platform.system()
+
+if system == "Darwin":
+    cmd = "mdfind -name node_modules | xargs dirname | grep -v '/node_modules$' | grep -v '/node_modules/' | grep -v '/\..*/' > paths.json && grep '^/Users' paths.json > tmp_paths.json && rm paths.json"
+
+elif system == "Linux":
+    cmd = "plocate -r '/node_modules$' | xargs dirname | grep -v '/node_modules$' | grep -v '/node_modules/' | grep -v '/\..*/' > paths.json && grep '^/home' paths.json > tmp_paths.json && rm paths.json"
+
+subprocess.call(cmd, shell=True)
+
+with open("tmp_paths.json", "r") as f:
+    paths = f.readlines()
+
+paths = [path.strip() for path in paths]
+paths_with_ids = []
+
+for path in paths:
+    id = base64.urlsafe_b64encode(os.urandom(6)).decode()
+    dependencies = {}
+    package_json_path = os.path.join(path, "package.json")
+    if os.path.exists(package_json_path):
+        with open(package_json_path, "r") as f:
+            package_json = json.load(f)
+        if "dependencies" in package_json:
+            dependencies.update(package_json["dependencies"])
+        if "devDependencies" in package_json:
+            dependencies.update(package_json["devDependencies"])
+        # if "peerDependencies" in package_json:
+        #     dependencies.update(package_json["peerDependencies"])
+        # if "optionalDependencies" in package_json:
+        #     dependencies.update(package_json["optionalDependencies"])
+        # if "bundledDependencies" in package_json:
+        #     dependencies.update(package_json["bundledDependencies"])
+        for key in dependencies.keys():
+            if dependencies[key].startswith('^'):
+                dependencies[key] = dependencies[key][1:]
+    paths_with_ids.append({"id": id, "path": path, "dependencies": dependencies})
+
+with open("paths2.json", "w") as f:
+    f.write(json.dumps(paths_with_ids, indent=4))
+
+os.remove("tmp_paths.json")
