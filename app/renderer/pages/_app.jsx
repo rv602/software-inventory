24b9516a0c69a332d0d1862f45662457632461db
rv602 <rajagopalan602@gmail.com>
2023-04-30 18:17:33 +0530

    js migration

--- app/renderer/pages/_app.jsx
+++ app/renderer/pages/_app.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import type { AppProps } from 'next/app';
-
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
