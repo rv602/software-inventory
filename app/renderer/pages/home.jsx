ab5f1a11f2dbcdcc78b26bf00f8efe88687a3e47
Abhinavxox <abhinavxox@gmail.com>
2023-04-30 18:32:31 +0530

    <Added>:[Back button and router instead of link]

--- app/renderer/pages/home.jsx
+++ app/renderer/pages/home.jsx
@@ -1,10 +1,13 @@
-import Link from "next/link";
+import Router from "next/router";
 
 const { exec } = require("child_process");
 export default function Home() {
   const runPythonScript = () => {
     exec(
-      "python3 scripts/python_environments.py && python3 scripts/node_environment.py"
+      "python3 scripts/python_environments.py && python3 scripts/node_environment.py",
+      (err, stdout, stderr) => {
+        Router.push("/view");
+      }
     );
   };
 
@@ -12,14 +15,12 @@ export default function Home() {
     <div className="flex justify-center items-center h-screen">
       <div className="flex flex-col justify-center items-center">
         <h1 className="text-5xl font-bold p-10">Software Inventory</h1>
-        <Link href="/view">
-          <button
-            className="bg-blue-500 p-3 rounded-md"
-            onClick={() => runPythonScript()}
-          >
-            Get Started
-          </button>
-        </Link>
+        <button
+          className="bg-blue-500 p-3 rounded-md"
+          onClick={() => runPythonScript()}
+        >
+          Get Started
+        </button>
       </div>
     </div>
   );
