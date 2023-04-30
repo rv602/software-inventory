484aa1a8aa3eee3a71cf7dae38507960158e78f3
rv602 <rajagopalan602@gmail.com>
2023-04-30 21:44:46 +0530

    minor table refactoring

--- app/renderer/pages/view.jsx
+++ app/renderer/pages/view.jsx
@@ -24,7 +24,7 @@ const DependencyTable = () => {
       <input
         type="text"
         placeholder="Search by path..."
-        className="w-full p-4 mb-8 rounded-md bg-gray-200 text-gray-800 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="w-full p-4 mb-8 rounded-md bg-gray-200 text-center text-gray-800 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
@@ -33,12 +33,12 @@ const DependencyTable = () => {
           <thead>
             <tr className="bg-gray-200">
               <th
-                className="px-4 py-2 border border-gray-500 text-left"
+                className="px-4 py-2 border border-gray-500 text-center"
                 style={{ minWidth: "300px" }}
               >
                 Project Name
               </th>
-              <th className="px-4 py-2 border border-gray-500 text-left">
+              <th className="px-4 py-2 border border-gray-500 text-center">
                 Dependencies
               </th>
             </tr>
@@ -46,7 +46,7 @@ const DependencyTable = () => {
           <tbody>
             {filteredDependencies.map((dependency) => (
               <tr key={dependency.id}>
-                <td className="px-4 py-2 border border-gray-500 text-sm font-medium text-gray-800">
+                <td className="px-4 py-2 border border-gray-500 text-sm font-medium text-center text-gray-800">
                   {dependency.path}
                 </td>
                 <td className="px-4 py-2 border border-gray-500 text-sm font-medium text-gray-800">
