a69f8d23f916903245d4238ee6f8f96890a9f717
Abhinavxox <abhinavxox@gmail.com>
2023-05-11 21:51:58 +0530

    <Added>:[Testing api for nvd in view]

--- app/renderer/pages/view.jsx
+++ app/renderer/pages/view.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Router from "next/router";
 import dependenciesData1 from "../../paths2.json";
 import dependenciesData2 from "../../paths.json";
+import axios from "axios";
 
 const DependencyTable = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -9,6 +10,55 @@ const DependencyTable = () => {
   const filteredDependencies = allDependencies.filter((dependency) =>
     dependency.path.toLowerCase().includes(searchTerm.toLowerCase())
   );
+
+  const [responses, setResponses] = useState([]);
+  const [dependencies, setDependencies] = useState({
+    bcryptjs: "2.4.3",
+    "body-parser": "1.20.2",
+    cloudinary: "1.35.0",
+    "cookie-parser": "1.4.6",
+    daisyui: "2.51.5",
+    dotenv: "16.0.0",
+    express: "3.0.0",
+    "express-fileupload": "1.4.0",
+    formidable: "2.1.1",
+    jsonwebtoken: "9.0.0",
+    mongoose: "7.0.0",
+    nodemailer: "6.9.1",
+    "react-js-pagination": "3.0.3",
+    "react-slick": "0.29.0",
+    "semantic-ui-css": "2.5.0",
+    "semantic-ui-react": "2.1.4",
+    validator: "13.9.0",
+    nodemon: "2.0.20",
+  });
+
+  const getAllVulnerabilities = async () => {
+    Object.keys(dependencies).forEach(async (key) => {
+      const response = await axios
+        .get(
+          `https://services.nvd.nist.gov/rest/json/cves/1.0?cpeMatchString=cpe%3A2.3%3Aa%3A%3A${key}%3A${dependencies[key]}&resultsPerPage=1000`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              api_key: "f1847978-47a2-47ed-96fa-04bf161e19ec",
+            },
+          }
+        )
+        .then((response) => {
+          if (response.data.totalResults > 0) {
+            setResponses((prevState) => [...prevState, response.data]);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    });
+  };
+
+  useEffect(() => {
+    getAllVulnerabilities();
+  }, [responses]);
 
   return (
     <div className="p-8">
@@ -64,6 +114,34 @@ const DependencyTable = () => {
             ))}
           </tbody>
         </table>
+      </div>
+      <div>
+        {responses.length > 0 ? (
+          <>
+            <p>
+              {responses.map((response) => {
+                return (
+                  <>
+                    <p>
+                      {response.result.CVE_Items.map((cve) => {
+                        return (
+                          <>
+                            <p>{cve.cve.CVE_data_meta.ID}</p>
+                            <p>
+                              {cve.cve.description.description_data[0].value}
+                            </p>
+                          </>
+                        );
+                      })}
+                    </p>
+                  </>
+                );
+              })}
+            </p>
+          </>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
